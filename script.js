14bca9614746a837614ae7ac11549259f7e79377
Joao <jphenriques.machado@gmail.com>
2024-03-28 17:40:10 -0300

    Aula2

---
+++ script.js
@@ -0,0 +1,29 @@
+let i = 0
+const BtnInserir = document.getElementById("Btn_Inserir")
+const Compras = document.getElementById("Compras")
+const Tarefas = document.getElementById("Tarefas")
+const Texto = document.getElementById("TxtTexto")
+
+BtnInserir.addEventListener("click", SetText)
+
+
+function SetText(){
+    const idLi = "Li_" + i;
+    const li = document.createElement("li")
+const btnRemove = document.createElement("button")
+    btnRemove.innerText = "Excluir"
+    li.id = idLi
+    const RemoverLi = () => {
+        document.getElementById(idLi).remove()
+    }
+    btnRemove.addEventListener("click", RemoverLi)
+    li.innerText = Texto.value
+    li.appendChild(btnRemove)
+    if(document.getElementById("radioCompras").checked){
+        Compras.appendChild(li)
+    }
+    if(document.getElementById("radioTarefas").checked){
+        Tarefas.appendChild(li)
+    }
+    i++
+}
